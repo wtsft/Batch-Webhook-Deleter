3c1501c0c617f3670e4cf9b8c4e3fea7ca221b87
Jose <78001618+Joseslol@users.noreply.github.com>
2022-05-11 14:46:56 -0600

    Add files via upload

---
+++ webhook deleter.bat
@@ -0,0 +1,10 @@
+@echo off
+:Main
+title Webhook Deleter
+echo Made By Jose
+color 04
+set /p input="Enter Your Webhook Url:"
+CURL -X "DELETE" %input%
+set /p input= Webhook SuccessFul Deleted
+cls
+goto main
